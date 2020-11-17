7dd19efaa7a9463cf8a0fd9160a6c5725a0a2afc
Mateus <53230135+MateusFerroAntunesdeOliveira@users.noreply.github.com>
2020-11-17 15:04:24 -0300

    Update DateAndTime_v3

--- DateAndTime_v3.c
+++ DateAndTime_v3.c
@@ -1,15 +1,15 @@
-#include <time.h>
-#include <conio.h>
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+int numero_dias[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+char *nome_mes[] = {"Janeiro", "Fevereiro", "Março", "Abril", 
+                    "Maio", "Junho", "Julho", "Agosto", "Setembro", 
+                    "Outubro", "Novembro", "Dezembro"};
 
 int main() {
 
     int i, ano, mes, dias_mes, d;
-    int numero_dias[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    char *nome_mes[] = {"Janeiro", "Fevereiro", "Março", "Abril", 
-                        "Maio", "Junho", "Julho", "Agosto", "Setembro", 
-                        "Outubro", "Novembro", "Dezembro"};
 
     // variável do tipo time_t para armazenar o tempo em t
     time_t t;
@@ -19,11 +19,11 @@ int main() {
     struct tm primeiro_dia;
 
     // pega tempo corrente e preenche na struct tm
-    t = time(NULL);
-    // time(&atual);
+    // t = time(NULL);
+    time(&t);
     atual = localtime(&t);
 
-    mes = atual->tm_mon;
+    mes = 1;
     ano = atual->tm_year;
 
     // Bissexto
@@ -37,12 +37,12 @@ int main() {
     primeiro_dia.tm_mon = mes;
     primeiro_dia.tm_year = ano;
 
+    // Cria uma data pegando campos da struct tm
     mktime(&primeiro_dia);
 
     printf("\n%s - %d\n", nome_mes[mes], ano + 1900);
 
     printf("DOM SEG TER QUA QUI SEX SAB\n");
-
     for (i = 0; i<primeiro_dia.tm_wday; i++) {
         printf("    ");
     }
